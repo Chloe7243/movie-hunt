c743c9d0b357a1540517d4b4da64cd0e6a0d017d
Stephanie <Stephaniechloe21@gmail.com>
2024-02-17 15:03:25 +0100

    Logo

--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = ({ dark }: { dark?: boolean }) => {
     <div className="flex gap-1 h-full items-center">
       <img src={logo} alt="" className="w-10 aspect-square object-contain" />
       <h1 className={`text-2xl font-bold leading-5 ${textColor}`}>
-        hunt <br /> movie
+        Movie <br /> <p style={{letterSpacing:".25rem"}}>Hunt</p>
       </h1>
       <hr />
     </div>
